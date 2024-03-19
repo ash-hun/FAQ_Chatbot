4972dbe603acf3c9b3e252fb3cf5f34e35ef1fc3
ash-hun <diadiahun0902@gmail.com>
2024-03-19 16:27:05 +0900

    main.py

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ import torch
 import os
 
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import Chroma
-from langchain.chains import RetrievalQA, ConversationChain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain import hub
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from operator import itemgetter
 
 from actions.model import getHFEmbedding
 
@@ -22,47 +20,73 @@ def load_data(file_path):
     data = dataLoader.load()
     return data
 
-def format_docs(docs):
-    # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
-    return "\n\n".join(doc.page_content for doc in docs)
+def document_Process(data):
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=10, length_function=len)
+    documents = text_splitter.split_documents(data)
+    return documents
+
+def create_DB(embeddingModel, docs=None):
+    # db = Chroma.from_documents(docs, embeddingModel, persist_directory="./data/DB_ko-sroberta-multitask/") ## Local DB 생성
+    db = Chroma(persist_directory="./data/DB_ko-sroberta-multitask/", embedding_function=embeddingModel)
+    return db
+
+def create_chain(db):
+    # LLM
+    llm = ChatOpenAI(temperature=0, model="gpt-4-turbo-preview", max_tokens=1024)
+    
+    # retriever
+    retriever=db.as_retriever(search_kwargs={'k': 1})
+    
+    # prompt
+    prompt = ChatPromptTemplate.from_template(
+        """You are an assistant for question-answering tasks from documents.
+        Use the following pieces of retrieved context to answer the question.
+        If question doesn't about the SmartStore, just say that "저는 스마트 스토어 FAQ를 위한 챗봇입니다. 스마트 스토어에 대한 질문을 부탁드립니다.".
+        the others, answer the question from documents.
+        
+        Context : {context}
+        Qusetion : {input}
+        """)
+
+    # stuff_chain
+    stuff_chain = create_stuff_documents_chain(
+        llm=llm,
+        prompt=prompt
+    )
+    
+    # Retrieval Chain
+    chain = create_retrieval_chain(
+        retriever,
+        stuff_chain
+    )
+
+    return chain
+
+def chat(chain, user_input):
+    response = chain.invoke({"input": user_input})
+    print("===" * 20)
+    print(f"[HUMAN]\n{user_input}\n")
+    print(f"[AI]\n{response['answer']}")
 
 if __name__ == "__main__":
     # Device Set
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
 
-    # Data Load
-    data = load_data('./data/Documents.csv')
-    
     # Check Model or Download
     embeddingModel = getHFEmbedding(hf_model="jhgan/ko-sroberta-multitask", device=device)
+    
+    # data = load_data('./data/Documents.csv')
+    # document_Process(data)
 
-    # LLM
-    llm = ChatOpenAI(temperature=0, model="gpt-4-turbo-preview", max_tokens=1024)
+    db = create_DB(embeddingModel)
+    QAchain = create_chain(db)
 
-    # text splitter
-    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=10, length_function=len)
-    documents = text_splitter.split_documents(data)
+    # Memory
+    # memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=1024)
+    # memory = ConversationBufferMemory(memory_key="chat_history")
+    # memory.load_memory_variables({})
 
-    # db = Chroma.from_documents(data, embeddingModel, persist_directory="./data/DB_ko-sroberta-multitask/") ## Local DB 생성
-    db = Chroma(persist_directory="./data/DB_ko-sroberta-multitask/", embedding_function=embeddingModel)
-
-    # retriever
-    retriever=db.as_retriever()
-
-    # prompt
-    prompt = hub.pull("rlm/rag-prompt")
-
-    query = "미성년자도 판매 회원 등록이 가능한가요?"
-
-    rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
-    )
-
-    response = rag_chain.invoke(query)
-
-    print("===" * 20)
-    print(f"[HUMAN]\n{query}\n")
-    print(f"[AI]\n{response}")
+    while True:
+        human_input = input()
+        if human_input != 'exit':
+            chat(QAchain, human_input)
